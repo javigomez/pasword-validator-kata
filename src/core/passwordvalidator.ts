d3ae71e8e0324364759d1f172e056eedc65884df
Javi G <javig@Javis-MacBook-Pro.local>
2023-05-17 08:48:21 +0200

    feat: check for invalid password

--- src/core/passwordvalidator.ts
+++ src/core/passwordvalidator.ts
@@ -1,4 +1,29 @@
-export function passwordValidator(expression:string): boolean {
-    return true
-}
-
+export function passwordValidator(password: string): boolean {
+    return (
+      longerThan6Chars(password) &&
+      containsNumber(password) &&
+      containsUppercaseLetter(password) &&
+      containsLowercaseLetter(password) &&
+      containsUnderscore(password)
+    );
+  }
+  
+  function longerThan6Chars(password: string): boolean {
+    return password.length >= 6;
+  }
+  
+  function containsNumber(password: string): boolean {
+    return /\d/.test(password);
+  }
+  
+  function containsUppercaseLetter(password: string): boolean {
+    return /[A-Z]/.test(password);
+  }
+  
+  function containsLowercaseLetter(password: string): boolean {
+    return /[a-z]/.test(password);
+  }
+  
+  function containsUnderscore(password: string): boolean {
+    return /_/.test(password);
+  }
--- src/tests/passwordvalidator.test.ts
+++ src/tests/passwordvalidator.test.ts
@@ -26,4 +26,19 @@ describe('passwordValidator', () => {
 	it('given a valid password returns true', () => {
 		expect(passwordValidator('1234abcdABCD_')).toBe(true)
 	})
+	it('given an short password returns false', () => {
+		expect(passwordValidator('1aA_')).toBe(false)
+	})
+	it('given an password without numbers returns false', () => {
+		expect(passwordValidator('abcdABCD_')).toBe(false)
+	})
+	it('given an password without capital letters returns false', () => {
+		expect(passwordValidator('1234abcd_')).toBe(false)
+	})
+	it('given an password without lowercase letters returns false', () => {
+		expect(passwordValidator('1234ABCD_')).toBe(false)
+	})
+	it('given an password without underscores letters returns false', () => {
+		expect(passwordValidator('1234abcdABCD')).toBe(false)
+	})
 })
